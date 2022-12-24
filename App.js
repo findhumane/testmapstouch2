b4d2af0ee33e666222b00c78b52f2adc3895be98
Find Humane <root@findhumane.com>
2022-12-24 17:16:50 -0600

    npx expo install react-native-map-clustering

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView from "react-native-map-clustering";
+import { Marker } from "react-native-maps";
 import { StyleSheet, View } from 'react-native';
 
+const markers = [
+  { id: 1, title: "Test1", description: "Hello World", location: { latitude: 30.270652526064772, longitude: -97.75368988587985 } },
+  { id: 2, title: "Test2", description: "Hello World", location: { latitude: 30.269826083021613, longitude: -97.75316677471446 } },
+  { id: 3, title: "Test3", description: "Hello World", location: { latitude: 30.270928005532998, longitude: -97.75273297521144 } },
+];
+
 export default function App() {
+  renderMarker = (marker) => (
+    <Marker
+      identifier={marker.id}
+      key={marker.id}
+      coordinate={marker.location}
+      title={marker.title}
+      description={marker.description}
+      tracksViewChanges={false}
+    />
+  );
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 30.26714,
+          longitude: -97.74259,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        { markers.map((marker) => renderMarker(marker)) }
+      </MapView>
     </View>
   );
 }
